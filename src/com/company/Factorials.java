c4265688e77b9feb58117264cabd92e015ac6254
KyleT62 <90464995+KyleT62@users.noreply.github.com>
2021-11-18 23:20:56 -0500

    Update Factorials.java

--- src/com/company/Factorials.java
+++ src/com/company/Factorials.java
@@ -7,7 +7,7 @@ public class Factorials {
             fact = fact * i;
         }
         return fact;
-    } //Find out why numbers don't work after about 12 or 13 factorial
+    }
 
     public double calcE(){
 
@@ -25,37 +25,31 @@ public class Factorials {
         e += 1.0 / hold;
 
         }
-        return e; //somehow move three decimal places over
+        return e;
     }
     public double calcEX(double x){
-
-        long hold;
-        double num;
-        int n = 2;
-        double inc = 0;
-        double e = 2.0;
+        double e = 1.0;
         double oldE = 0;
+        double count = 1;
+        double exp;
 
         while (e - oldE > 0.001)
         {
-
             oldE = e;
-            hold = calcFactorial(n);
-            n++;
-            num = Math.pow(x,inc);
-            inc++;
-            e += (num / hold);
+            exp = Math.pow(x,count);
+            e += exp / calcFactorial((int) count);
+            count++;
         }
-        return e; //fix
+        return e;
     }
 
     public static void main(String[] args) {
         Factorials test = new Factorials();
 
-        int n = 0;
+        int n = 1;
         for (int i = 1; i <= 20; i++){
-            n++;
             System.out.println(test.calcFactorial(n));
+            n++;
         }
 
         System.out.println();
@@ -64,12 +58,44 @@ public class Factorials {
 
         System.out.println();
 
-        int a = 0;
-        for (int i = 1; i <= 5; i++){
-            a++;
+        int a = 1;
+        for (int i = 1; i < 6; i++){
             System.out.println(test.calcEX(a));
+            a++;
         }
     }
-
-
 }
+
+/*
+OUTPUT
+
+1
+2
+6
+24
+120
+720
+5040
+40320
+362880
+3628800
+39916800
+479001600
+6227020800
+87178291200
+1307674368000
+20922789888000
+355687428096000
+6402373705728000
+121645100408832000
+2432902008176640000
+
+2.7182539682539684
+
+2.7182539682539684
+7.388994708994708
+20.08546859390609
+54.5978829056501
+148.4129510721643
+
+ */

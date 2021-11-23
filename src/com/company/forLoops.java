94c6336cb8f62091491249d4c603f32c3e5cabf6
KyleT62 <90464995+KyleT62@users.noreply.github.com>
2021-11-22 22:26:47 -0500

    Update forLoops.java

--- src/com/company/forLoops.java
+++ src/com/company/forLoops.java
@@ -17,8 +17,46 @@ public class forLoops {
     }
 
     public void sillyNumbers() {
-        for(int i = 0; i < 9; i++)
-            for(int b = i; b )
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k < 3; k++)
+                    System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public void sillyNumbers2() {
+        for (int i = 0; i < 3; i++) {
+            for (int k = 9; k > 0; k--) {
+                for (int j = k; j > 0; j--)
+                    System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+
+    public void dollarsAndStars() {
+        for (int i = 7; i > 0; i--){
+            for ( int t = 7; t > i; t--){
+                System.out.print("**");
+            }
+            for (int a = 0; a < i; a++) {
+                System.out.print("$");
+            }
+            for ( int y = i; y > 0; y--){
+                System.out.print("*");
+            }
+            for (int b = 0; i > b; b++)
+                System.out.print("*");
+            for (int j = i; j > 0; j--){
+                System.out.print("$");
+            }
+            for (int k = 7; k > i; k--) {
+                System.out.print("**");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
@@ -33,5 +71,49 @@ public class forLoops {
         System.out.println();
 
         test.printSquaresBack(8);
+
+        System.out.println();
+
+        test.sillyNumbers();
+
+        System.out.println();
+
+        test.sillyNumbers2();
+
+        System.out.println();
+
+        test.dollarsAndStars();
     }
 }
+
+/*
+OUTPUT
+12
+20
+20
+
+64
+49
+36
+25
+16
+9
+4
+1
+
+000111222333444555666777888999
+000111222333444555666777888999
+000111222333444555666777888999
+
+999999999888888887777777666666555554444333221
+999999999888888887777777666666555554444333221
+999999999888888887777777666666555554444333221
+
+$$$$$$$**************$$$$$$$
+**$$$$$$************$$$$$$**
+****$$$$$**********$$$$$****
+******$$$$********$$$$******
+********$$$******$$$********
+**********$$****$$**********
+************$**$************
+ */

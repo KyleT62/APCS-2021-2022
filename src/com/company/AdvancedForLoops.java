f689dc6ed9aac2c3e20cce9acc016387bfdc1b63
KyleT62 <90464995+KyleT62@users.noreply.github.com>
2021-11-23 21:25:18 -0500

    Finished

--- src/com/company/AdvancedForLoops.java
+++ src/com/company/AdvancedForLoops.java
@@ -14,12 +14,12 @@ public class AdvancedForLoops {
     }
     public static void alternate(int n){
         for(int i = 1; i <= n; i++){
-                for (int j = 1; j <= i && i % 2 != 0; j++) {
-                    System.out.print(j);
-                }
-                for (int k = i; k >= 1 && i % 2 == 0; k--){
-                    System.out.print(k);
-                }
+            for (int j = 1; j <= i && i % 2 != 0; j++) {
+                System.out.print(j);
+            }
+            for (int k = i; k >= 1 && i % 2 == 0; k--){
+                System.out.print(k);
+            }
             System.out.println();
         }
     }
@@ -42,9 +42,9 @@ public class AdvancedForLoops {
     public static void upSideDown(int end, int start) {
         for(int i = 1; i <= start; i++){
             for(int k = i - 1; k >= 1; k--){
-                System.out.print("t"); //change t back to open space
+                System.out.print(" ");
             }
-            for(int b = end; b - i -1 >= start; b-- ){
+            for(int b = end - i + 1; b> start; b-- ){
                 System.out.print(b);
             }
             for(int j = start; j <= end - i + 1; j++){
@@ -63,10 +63,50 @@ public class AdvancedForLoops {
 
         System.out.println();
 
+        alternate(5);
+
+        System.out.println();
+
         isosceles(5);
 
         System.out.println();
 
         upSideDown(9,5);
+
+        System.out.println();
+
+        upSideDown(8,6);
     }
 }
+
+/*
+OUTPUT
+
+ 666666
+  4444
+   22
+
+1
+21
+123
+4321
+12345
+654321
+
+    1
+   121
+  12321
+ 1234321
+123454321
+
+987656789
+ 8765678
+  76567
+   656
+    5
+
+87678
+ 767
+  6
+
+ */
